47ebff84a7930da3eed4d7f11a6fb0c4ec2a5ac9
mario <marioarsw@gmail.com>
2021-07-26 19:55:37 -0600

    Variables

---
+++ js/01. Variables.js
@@ -0,0 +1,21 @@
+//Variables
+
+var producto = 'Audifonos gamer'; //Iniciar la variable y asignarle valor
+
+var disponible; //Iniciar la variable sin darle valor
+
+producto = true; //Reasignando el valor de la variable
+
+disponible = true; //Asignando valor a la variable
+
+var producto = 'Computadora',
+    disponible1 = true,
+    categoria = 'Computadoras';
+
+//Estilos para las variables
+var nombre_producto = 'Monitor HD' //underscore
+var nombreProducto = 'Monitor HD' //camelCase
+var NombreProducto = 'Monitor HD' //PascalCase
+
+
+console.log(producto);
