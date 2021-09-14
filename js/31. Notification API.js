c610ba3250a17fffc1ea8182b0ef6940d6b0d880
mario <marioarsw@gmail.com>
2021-09-13 22:46:09 -0600

    notification api

--- js/31. Notification API.js
+++ js/31. Notification API.js
@@ -1,3 +1,16 @@
 /**
- * Cuando activas las notificaciones de yt cuando alguien sube nuevos videos
+ * Cuando activas las notificaciones de yt llegarán las notificaciones de alguien sube nuevos videos
  */
+
+const boton = document.querySelector("#boton");
+boton.addEventListener("click", () => {
+    Notification.requestPermission()
+        .then(resultado => console.log( `El resultado es ${resultado}`));
+});
+
+if(Notification.permission == "granted") {
+    new Notification("Esta es una notificación", {
+        icon: "img/ccj.png",
+        body: "Code con Juan"
+    })
+}
